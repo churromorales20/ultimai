ffffa0f150902a25590405c771e0f75abdabff21
Cecilio Morales <cecilio.morales@leniolabs.com>
2025-07-10 22:48:45 -0500

    Missing db change

--- src/services/rule_answers_service.py
+++ src/services/rule_answers_service.py
@@ -1,34 +1,35 @@
-from database.db import engine, SessionLocal, Base
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from database.db import db_manager
 from models.answered_question import AnsweredQuestion
 
-Base.metadata.create_all(bind=engine)
-
 class RuleAnswerSrvc:
+  
   @classmethod  
-  def loadPrevious(cls, identifier):
-    db = cls.get_db()
+  def loadPrevious(cls, db, identifier):
     
     return db.query(AnsweredQuestion).filter(AnsweredQuestion.identifier == identifier).order_by(AnsweredQuestion.created_at.asc()).all()
- 
+  
   @classmethod  
-  def save(cls, question, response, identifier):
-    db = cls.get_db()
+  def save(cls, db, question, response, identifier):
     rule_answered = AnsweredQuestion(
-       question=question, 
-       rule=response.rule,
-       identifier=identifier,
-       explanation=response.explanation,
-       signals=response.signals,
-       short_answer=response.short_answer
+        question=question, 
+        rule=response.rule,
+        identifier=identifier,
+        explanation=response.explanation,
+        signals=response.signals,
+        short_answer=response.short_answer
     )
+
     db.add(rule_answered)
     db.commit()
     db.refresh(rule_answered)
 
     return rule_answered.id
+  
   @classmethod  
-  def add_feeback(cls, feedback_request):
-    db = cls.get_db()
+  def add_feeback(cls, db, feedback_request):
+
     answered_question = db.query(AnsweredQuestion).filter(AnsweredQuestion.id == feedback_request.answer_id).first()
 
     if answered_question:
@@ -42,11 +43,6 @@ class RuleAnswerSrvc:
         db.commit()
         db.refresh(answered_question)
         return answered_question
+    
     else:
         return None
-  def get_db():
-      db = SessionLocal()
-      try:
-          return db
-      finally:
-          db.close()
